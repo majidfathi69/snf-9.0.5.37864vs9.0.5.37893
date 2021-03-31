d5aee4fcc251df4ae7ed2d12130084ebc2f635cc
majidfathi69 <majidfathi69@gmail.com>
2021-03-31 22:39:07 +0430

    second commit

--- Offsets/1Offsets.cs
+++ Offsets/1Offsets.cs
@@ -2,7 +2,7 @@
 {
   public class NetClient
   {
-    public static int Ptr = 56261640;
+    public static int Ptr = 55053320;
     public static int SessionKey = 555;
     public static int ZStreamPtr = 432;
   }
--- Offsets/Offsets.cs
+++ Offsets/Offsets.cs
@@ -3,6 +3,6 @@
   public class Misc
   {
     public static string Patch = "9.0.5";
-    public static int Build = 37864;
+    public static int Build = 37893;
   }
 }
